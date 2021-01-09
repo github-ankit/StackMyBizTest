356b3ba18ac53bd9f300ce1b024afa8e06d6f2bd
Ankit Singh <ankit.4uv@gmail.com>
2021-01-09 19:40:23 +0530

    Added Empty field validation and email field validation

--- app/src/main/java/com/liveup/stackmybiztest/LoginActivity.java
+++ app/src/main/java/com/liveup/stackmybiztest/LoginActivity.java
@@ -1,16 +1,17 @@
 package com.liveup.stackmybiztest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewbinding.ViewBinding;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.os.Handler;
 import android.view.View;
 
 import com.liveup.stackmybiztest.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +20,63 @@ public class LoginActivity extends AppCompatActivity {
         ActivityLoginBinding binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Utils.CheckConnectivty(this);
+
         binding.forgotPasswordTV.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,UserProfileActivity.class));
+                startActivity(new Intent(LoginActivity.this, UserProfileActivity.class));
             }
         });
 
         binding.loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this,UserProfileActivity.class));
+
+                if (Utils.CheckConnectivty(LoginActivity.this)) {
+
+                    if (binding.userNamEmailET.getText().toString().isEmpty()) {
+                        Utils.showToast(LoginActivity.this, "Enter email id");
+                    } else if (!Utils.isValidEmail(binding.userNamEmailET.getText().toString())) {
+                        Utils.showToast(LoginActivity.this, "Enter valid email id");
+                    } else if (binding.passwordET.getText().toString().isEmpty()) {
+                        Utils.showToast(LoginActivity.this, "Enter password");
+                    } else {
+                        startActivity(new Intent(LoginActivity.this, UserProfileActivity.class));
+                    }
+
+                }
+
+
             }
         });
 
 
     }
+
+    @Override
+    public void onBackPressed() {
+
+        if (!this.doubleBackToExitPressedOnce) {
+            this.doubleBackToExitPressedOnce = true;
+            Utils.showToast(this, "Press again to close app");
+
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    doubleBackToExitPressedOnce = false;
+                }
+            }, 5000);
+
+        } else {
+
+            super.onBackPressed();
+
+            finishAffinity();
+
+
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/liveup/stackmybiztest/Utils.java
@@ -0,0 +1,76 @@
+package com.liveup.stackmybiztest;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.Gravity;
+import android.widget.Toast;
+
+
+public class Utils {
+
+    public static void showToast(Context context, String message) {
+
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_HORIZONTAL | Gravity.CENTER_VERTICAL, 0, 0);
+        toast.show();
+    }
+
+
+    public static boolean isValidEmail(String target) {
+        return (!TextUtils.isEmpty(target) && Patterns.EMAIL_ADDRESS.matcher(target).matches());
+    }
+
+
+    public static Boolean CheckConnectivty(Activity activity) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if (activeNetworkInfo == null || !activeNetworkInfo.isConnected()) {
+            promptInternetConnect(activity);
+            return false;
+
+        } else {
+
+            //Yes there is active internet connection.
+            return true;
+        }
+
+
+    }
+
+    /**
+     * Show A Dialog with button to refresh the internet state.
+     */
+    private static void promptInternetConnect(final Activity activity) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        builder.setTitle("Connectivty");
+        builder.setMessage("No Internet Connection");
+        builder.setCancelable(false);
+
+        String positiveText = "Refresh";
+        builder.setPositiveButton(positiveText,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+
+                        CheckConnectivty(activity);
+
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+
+    }
+
+
+}
